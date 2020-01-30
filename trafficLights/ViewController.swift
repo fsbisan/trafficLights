d0a01219b973823c34bdeb6e163468d853ccb19f
AleksMakarov <fsbisan@mail.ru>
2020-01-30 09:23:26 +0500

    Red, Green, Blue light

--- trafficLights/ViewController.swift
+++ trafficLights/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var blueLight: UIView!
+    @IBOutlet var switchButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.layer.cornerRadius = 60
+        redLight.backgroundColor = UIColor.init(red: 1, green: 0, blue: 0, alpha: 0.3)
+        greenLight.layer.cornerRadius = 60
+        greenLight.backgroundColor = UIColor.init(red: 0, green: 1, blue: 0, alpha: 0.3)
+        blueLight.layer.cornerRadius = 60
+        blueLight.backgroundColor = UIColor.init(red: 0, green: 0, blue: 1, alpha: 0.3)
+        switchButton.setTitle("Start", for: .normal)
+        switchButton.layer.cornerRadius = 7
     }
-
+    @IBAction func buttonTouch() {
+    }
+    
 
 }
 
