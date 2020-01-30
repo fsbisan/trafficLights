2860eb403acbcbaa896846750242e44464efe89c
AleksMakarov <fsbisan@mail.ru>
2020-01-30 18:23:25 +0500

    Remastered code!

--- trafficLights/ViewController.swift
+++ trafficLights/ViewController.swift
@@ -9,51 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    //MARK: - IBOutlet
     @IBOutlet var redLight: UIView!
     @IBOutlet var greenLight: UIView!
     @IBOutlet var blueLight: UIView!
     @IBOutlet var switchButton: UIButton!
+    var lightOn: CGFloat = 1
+    var lightOf: CGFloat = 0.3
     override func viewDidLoad() {
         super.viewDidLoad()
-        redLight.layer.cornerRadius = 60
-        redLight.backgroundColor = UIColor.red.withAlphaComponent(0.3)
-        greenLight.layer.cornerRadius = 60
-        greenLight.backgroundColor = UIColor.green.withAlphaComponent(0.3)
-        blueLight.layer.cornerRadius = 60
-        blueLight.backgroundColor = UIColor.blue.withAlphaComponent(0.3)
+        
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        redLight.backgroundColor = UIColor.red.withAlphaComponent(lightOf)
+        
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+        greenLight.backgroundColor = UIColor.green.withAlphaComponent(lightOf)
+        
+        blueLight.layer.cornerRadius = blueLight.frame.width / 2
+        blueLight.backgroundColor = UIColor.blue.withAlphaComponent(lightOf)
+        
         switchButton.setTitle("Start", for: .normal)
         switchButton.layer.cornerRadius = 7
     }
+    //MARK: - IBActions
     @IBAction func buttonTouch() {
-        if redLight.backgroundColor == UIColor.red.withAlphaComponent(0.3) &&
-            greenLight.backgroundColor == UIColor.green.withAlphaComponent(0.3) &&
-            blueLight.backgroundColor == UIColor.blue.withAlphaComponent(0.3)
-        {
-            redLight.backgroundColor = UIColor.red.withAlphaComponent(1)
+        if redLight.alpha == lightOf, redLight.alpha == lightOf, blueLight.alpha == lightOf {
+            redLight.alpha = lightOn
             switchButton.setTitle("Next", for: .normal)
-        }
-        else if redLight.backgroundColor == UIColor.red.withAlphaComponent(1) &&
-            greenLight.backgroundColor == UIColor.green.withAlphaComponent(0.3) &&
-            blueLight.backgroundColor == UIColor.blue.withAlphaComponent(0.3)
-        {
-            greenLight.backgroundColor = UIColor.green.withAlphaComponent(1)
-            redLight.backgroundColor = UIColor.red.withAlphaComponent(0.3)
-            
-        }
-        else if redLight.backgroundColor == UIColor.red.withAlphaComponent(0.3) &&
-            greenLight.backgroundColor == UIColor.green.withAlphaComponent(1) &&
-            blueLight.backgroundColor == UIColor.blue.withAlphaComponent(0.3)
-        {
-            greenLight.backgroundColor = UIColor.green.withAlphaComponent(0.3)
-            blueLight.backgroundColor = UIColor.blue.withAlphaComponent(1)
-        }
-        else if redLight.backgroundColor == UIColor.red.withAlphaComponent(0.3) &&
-            greenLight.backgroundColor == UIColor.green.withAlphaComponent(0.3) &&
-            blueLight.backgroundColor == UIColor.blue.withAlphaComponent(1)
-        {
-            redLight.backgroundColor = UIColor.red.withAlphaComponent(1)
-            blueLight.backgroundColor = UIColor.blue.withAlphaComponent(0.3)
+        } else if redLight.alpha == lightOn {
+            greenLight.alpha = lightOn
+            redLight.alpha = lightOf
+        } else if greenLight.alpha == lightOn {
+            greenLight.alpha = lightOf
+            greenLight.alpha = lightOn
+        } else if blueLight.alpha == lightOn {
+            blueLight.alpha = lightOf
+            redLight.alpha = lightOn
         }
     }
     
