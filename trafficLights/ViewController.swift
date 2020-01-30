11bdb34b58c5e12a2f0f693fe8ad205436fd799a
AleksMakarov <fsbisan@mail.ru>
2020-01-30 11:18:18 +0500

    All done!

--- trafficLights/ViewController.swift
+++ trafficLights/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var redLight: UIView!
     @IBOutlet var greenLight: UIView!
     @IBOutlet var blueLight: UIView!
@@ -17,17 +17,46 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         redLight.layer.cornerRadius = 60
-        redLight.backgroundColor = UIColor.init(red: 1, green: 0, blue: 0, alpha: 0.3)
+        redLight.backgroundColor = UIColor.red.withAlphaComponent(0.3)
         greenLight.layer.cornerRadius = 60
-        greenLight.backgroundColor = UIColor.init(red: 0, green: 1, blue: 0, alpha: 0.3)
+        greenLight.backgroundColor = UIColor.green.withAlphaComponent(0.3)
         blueLight.layer.cornerRadius = 60
-        blueLight.backgroundColor = UIColor.init(red: 0, green: 0, blue: 1, alpha: 0.3)
+        blueLight.backgroundColor = UIColor.blue.withAlphaComponent(0.3)
         switchButton.setTitle("Start", for: .normal)
         switchButton.layer.cornerRadius = 7
     }
     @IBAction func buttonTouch() {
+        if redLight.backgroundColor == UIColor.red.withAlphaComponent(0.3) &&
+            greenLight.backgroundColor == UIColor.green.withAlphaComponent(0.3) &&
+            blueLight.backgroundColor == UIColor.blue.withAlphaComponent(0.3)
+        {
+            redLight.backgroundColor = UIColor.red.withAlphaComponent(1)
+            switchButton.setTitle("Next", for: .normal)
+        }
+        else if redLight.backgroundColor == UIColor.red.withAlphaComponent(1) &&
+            greenLight.backgroundColor == UIColor.green.withAlphaComponent(0.3) &&
+            blueLight.backgroundColor == UIColor.blue.withAlphaComponent(0.3)
+        {
+            greenLight.backgroundColor = UIColor.green.withAlphaComponent(1)
+            redLight.backgroundColor = UIColor.red.withAlphaComponent(0.3)
+            
+        }
+        else if redLight.backgroundColor == UIColor.red.withAlphaComponent(0.3) &&
+            greenLight.backgroundColor == UIColor.green.withAlphaComponent(1) &&
+            blueLight.backgroundColor == UIColor.blue.withAlphaComponent(0.3)
+        {
+            greenLight.backgroundColor = UIColor.green.withAlphaComponent(0.3)
+            blueLight.backgroundColor = UIColor.blue.withAlphaComponent(1)
+        }
+        else if redLight.backgroundColor == UIColor.red.withAlphaComponent(0.3) &&
+            greenLight.backgroundColor == UIColor.green.withAlphaComponent(0.3) &&
+            blueLight.backgroundColor == UIColor.blue.withAlphaComponent(1)
+        {
+            redLight.backgroundColor = UIColor.red.withAlphaComponent(1)
+            blueLight.backgroundColor = UIColor.blue.withAlphaComponent(0.3)
+        }
     }
     
-
+    
 }
 
